913ecbc92d50f710ab32227089835ee281ffbca2
SimplePleasures <xuxanwan@gmail.com>
2014-11-20 11:19:01 +0800

    单元2提交

---
+++ 2.11/1/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+this is my first java program
+*/
+public class HelloWorld{
+/**
+this is the entrance of the class
+*/
+	public static void main(String args[]){
+		/**this is a output satement*/
+		System.out.println("Hello World");
+	}
+}
