0b6f6f4bf6e0676aee9d28ba95f8e0cf67e09485
SimplePleasures <xuxanwan@gmail.com>
2015-05-07 10:08:49 +0800

    add Ex16.java

--- code/holding/Ex16.java
+++ code/holding/Ex16.java
@@ -1,5 +1,33 @@
 package holding;
 
-public class Ex16 {
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
+import net.mindview.util.TextFile;
+
+public class Ex16 {
+    private final static String vowels = "aeiouAEIOU"; 
+    public static void main(String[] args) {
+        Set<String> words = new TreeSet<String>(new TextFile("./code/holding/SetOperations.java", "\\W+"));
+        
+        Set<String> vowel = new HashSet<String>();
+
+        Iterator<String> iter = words.iterator();
+        int totalCount = 0;
+        while (iter.hasNext()) {
+            String wordTemp = (String) iter.next();
+            char[] wordTempArray = wordTemp.toCharArray();
+            for (int i = 0; i < wordTempArray.length; i++) {
+                int count = 0;
+                if (vowels.contains(wordTempArray[i]+"")) {
+                    count ++;
+                    totalCount++;
+                }
+                System.out.println(count);
+            }
+        }
+        System.out.println(totalCount);
+    }
 }
---
+++ code/holding/Ex16_1.java
@@ -0,0 +1,32 @@
+package holding;
+
+import java.util.*;
+import net.mindview.util.*;
+
+public class Ex16_1 {
+    static void vowelCounter(Set<String> st) {
+        Set<Character> vowels = new TreeSet<Character>();
+        Collections.addAll(vowels, 
+            'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
+        int allVowels = 0;
+        for(String s : st) {
+            int count = 0;
+            for(Character v : s.toCharArray()) {        
+                if(vowels.contains(v)) {
+                    count++;
+                    allVowels++; 
+                }
+            }
+            System.out.print(s + ": " + count + ", ");      
+        }
+        System.out.println();   
+        System.out.print("Total vowels: " + allVowels);
+    }
+    public static void main(String[] args) {
+        Set<String> words = new TreeSet<String>(
+            new TextFile("SetOperations.java", "\\W+"));
+        System.out.println(words);
+        System.out.println();
+        vowelCounter(words);        
+    }       
+}
